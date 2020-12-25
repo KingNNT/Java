dd1a6fea910e74c3525258f81b1dc18efe7b5990
KingNNT <Dev.KingNNT@gmail.com>
2020-12-25 11:44:45 +0700

    Update Thread SystemPrograming

--- Core/SystemPrograming/src/com/company/Main.java
+++ Core/SystemPrograming/src/com/company/Main.java
@@ -1,34 +1,14 @@
 package com.company;
 
 import java.lang.*;
-import java.util.*;
-import java.io.*;
 
 public class Main {
+    private static void solution() {
+        ThreadReadWriteFile threadFirst = new ThreadReadWriteFile();
+        threadFirst.start();
+    }
+
     public static void main(String[] args) {
-        try {
-            FileWriter fileWriter = new FileWriter("/mnt/LearningAndWorking/Develop/Java/Core/SystemPrograming/output.txt");
-            Scanner scanner = new Scanner(System.in);
-            String readString = scanner.nextLine();
-            while(readString!=null) {
-                System.out.println(readString);
-                fileWriter.write(readString + "\n");
-
-                if (readString.isEmpty()) {
-                    System.out.println("Read Enter Key.");
-                    break;
-                }
-
-                if (scanner.hasNextLine()) {
-                    readString = scanner.nextLine();
-                } else {
-                    readString = null;
-                }
-            }
-            fileWriter.close();
-        } catch (IOException e) {
-            System.out.println("An error occured.");
-            e.printStackTrace();
-        }
+        solution();
     }
 }
