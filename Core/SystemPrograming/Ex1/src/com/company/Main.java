71247c44c4223562f75765651ddc34b1a048338f
KingNNT <Dev.KingNNT@gmail.com>
2021-01-05 18:42:30 +0700

    Update Ex1

--- Core/SystemPrograming/Ex1/src/com/company/Main.java
+++ Core/SystemPrograming/Ex1/src/com/company/Main.java
@@ -3,7 +3,9 @@ import com.company.Task1;
 public class Main {
     private static void solution() {
         Task1 t1 = new Task1();
+        Task2 t2 = new Task2();
         t1.start();
+        t2.start();
     }
     public static void main(String[] args) {
         solution();
